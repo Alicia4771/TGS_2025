a5ad23e4e7fbe729982ed8ba2bfb6c52d3f3ce15
Alicia4771 <rinn1204@icloud.com>
2025-07-17 11:03:23 +0900

    ScriptableObjectの練習

---
+++ Assets/Script/ShareDataScriptableObject.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScoreScriptableObject", menuName = "ScoreScriptableObject", order = 0)]
+public class ShareDataScriptableObject : UnityEngine.ScriptableObject
+{
+    public double MoveRatio;
+}
